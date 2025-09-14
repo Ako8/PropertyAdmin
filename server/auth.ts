25e9b68a4898dd9adb5e6a03d37c8355f5ce7bae
git-for-work <akogachechiladze22@gmail.com>
2025-09-14 14:16:15 +0400

    Update session secret in authentication setup for improved security

--- server/auth.ts
+++ server/auth.ts
@@ -40,7 +40,7 @@ async function authenticateWithExternalAPI(username: string, password: string): 
 
 export function setupAuth(app: Express) {
   const sessionSettings: session.SessionOptions = {
-    secret: process.env.SESSION_SECRET!,
+    secret: "uifergwuwgaswerghnohdergno",
     resave: false,
     saveUninitialized: false,
     store: storage.sessionStore,
